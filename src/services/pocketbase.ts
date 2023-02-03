d3e9fc7cd1373f3af3d5070e7e8e9a30be9aeadc
hermione <hermione@hog.uk>
2023-02-03 18:49:13 +0500

    добавил pocketbase и уговорил отправить запрос на авторизацию

---
+++ src/services/pocketbase.ts
@@ -0,0 +1,5 @@
+import PocketBase from 'pocketbase';
+
+const client = new PocketBase("http://37.79.255.134:20000");
+
+export default client;
--- src/views/login/loginPage.tsx
+++ src/views/login/loginPage.tsx
@@ -1,32 +1,43 @@
 import React from "react";
 import BasicLayout from "../../components/basicLayout";
-import {Box, Center, Heading, VStack} from "@chakra-ui/react";
+import { Box, Center, Heading, VStack } from "@chakra-ui/react";
 import LoginForm from "../../components/loginForm";
-import {IFormFields} from "../../components/loginForm/types";
-
+import { IFormFields } from "../../components/loginForm/types";
+import client from "../../services/pocketbase";
+import { redirect } from "react-router-dom";
 
 const LoginPage: React.FC = () => {
-
-
-    const onLogin = async (values: IFormFields) => {
-        console.log(values)
-        return {success: true}
-    }
-    return (
-        <BasicLayout>
-            <Center w='100%' h='100%'>
-                <Box w='400px' h='350px' bg='gray.700' borderRadius='18px'>
-                    <VStack h='100%' justifyContent='space-between' alignItems='center'>
-                        <Heading mt={5} size='lg'>Добро пожаловать!</Heading>
-                        <Center w='100%'>
-                            <Box w='80%' mb={20}>
-                                <LoginForm onFormSubmit={onLogin}/>
-                            </Box>
-                        </Center>
-                    </VStack>
-                </Box>
+  const onLogin = async (values: IFormFields) => {
+    // authWithPassword автоматически кладет данные о пользователе в local storage
+    // https://pocketbase.io/docs/authentication
+    client
+      .collection("users")
+      .authWithPassword(values.login, values.password)
+      .then(() => {
+        redirect("/");
+      })
+      .finally(() => {
+        // stop loading
+      });
+    return { success: true };
+  };
+  return (
+    <BasicLayout>
+      <Center w="100%" h="100%">
+        <Box w="400px" h="350px" bg="gray.700" borderRadius="18px">
+          <VStack h="100%" justifyContent="space-between" alignItems="center">
+            <Heading mt={5} size="lg">
+              Добро пожаловать!
+            </Heading>
+            <Center w="100%">
+              <Box w="80%" mb={20}>
+                <LoginForm onFormSubmit={onLogin} />
+              </Box>
             </Center>
-        </BasicLayout>
-    )
-}
+          </VStack>
+        </Box>
+      </Center>
+    </BasicLayout>
+  );
+};
 export default LoginPage;
